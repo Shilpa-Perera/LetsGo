be52d383021b3011bb284e00ed8e518283f3f684
Shilpa-Perera <laksahaniperera@gmail.com>
2024-04-10 17:29:06 +0530

    Set reference points on image

---
+++ app/src/main/java/com/example/letsgo/MapInfo.java
@@ -0,0 +1,16 @@
+package com.example.letsgo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.View;
+
+public class Map {
+    String imageURI ;
+    int mapHeight ;
+    int mapName ;
+    
+
+}
